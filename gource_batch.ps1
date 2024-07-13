ac125669f645aa6953c268168ed7d47cf151f023
yettey74 <golith@hotmail.com>
2024-07-13 18:48:57 +0930

    Updating Gource Availablity to pipe video

--- gource_batch.ps1
+++ gource_batch.ps1
@@ -13,8 +13,12 @@ Get-Content log2.txt | ForEach-Object { $_ -replace '^(.+)\|', '$1|/gameServer|'
 #>
 
 # Combine the logs
-$log1 = Get-Content log1_modified.txt
-$log2 = Get-Content log2_modified.txt
+# $log1 = Get-Content log1_modified.txt
+# $log2 = Get-Content log2_modified.txt
+# $combined = $log1 + $log2
+
+$log1 = Get-Content log1.txt
+$log2 = Get-Content log2.txt
 $combined = $log1 + $log2
 
 # Sort the combined logs by the timestamp
@@ -25,5 +29,17 @@ $sortedCombined = $combined | Sort-Object {
 # Save the sorted logs
 $sortedCombined | Set-Content combined.txt
 
+git log --pretty="%at|%as %s" > captions.txt
+
 # Visualize with Gource, adjusting settings for complete tree display
 gource combined.txt --auto-skip-seconds 1 --max-files 10000
+
+gource combined.txt --auto-skip-seconds 1 --max-files 10000 --output-ppm-stream - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i pipe: -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 output.mp4
+
+gource --load-config gource.config --output-ppm-stream - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i pipe: -vcodec libx264 -preset medium -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 output/gource_intermediary.mp4
+
+# Merge of both methods
+gource --load-config gource.config combined.txt --output-ppm-stream - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i pipe: -vcodec libx264 -preset medium -pix_fmt yuv420p -crf 1 -threads 0 output.mp4
+
+
+gource --load-config gource.config --output-ppm-stream - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i pipe: -vcodec libx264 -preset medium -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 output/gource_intermediary.mp4
